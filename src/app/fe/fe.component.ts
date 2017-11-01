3b2165d90d271f55c356505d418b2082d25bb781
sharath <sharath.r@ge.com>
2017-11-01 18:20:57 +0530

    version_1

--- src/app/fe/fe.component.ts
+++ src/app/fe/fe.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Injectable } from '@angular/core';
 import { Http, Response, RequestOptions, Headers } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
@@ -10,42 +10,24 @@ import 'rxjs/add/operator/map';
     templateUrl: './fe.component.html',
     styleUrls: ['./fe.component.css']
 })
-
 export class FECompoment {
-    people: any;
-    constructor(private http: Http) {
-        this.people = "";
-        // this.http.get(`http://devl-smartfe.cloud.health.ge.com/ServiceOracleAPI/inventory/ping`)
-        //     .map((res: Response) => res.json())
-        //     .subscribe(people => this.people = people);
-    }
-
-    fetchdata() {
-        console.log("Fetch Data")
-        this.http.get(`http://localhost:9099/ping`)
-            .map((res: Response) => res.json())
-            .subscribe(res => this.people = res);
-
-
-        return this.http.get(`http://localhost:9099/ping`)
-            .map((res: Response) => JSON.stringify(res.json()));
-    }
-
-    testDev() {
-        // this.http.get((`http://devl-smartfe.cloud.health.ge.com/ServiceOracleAPI/inventory/ping`)
-        //     .map((res: Response) => res.json())
-        //     .subscribe(people => this.people = people);
-
-        return this.http.get(`http://devl-smartfe.cloud.health.ge.com/ServiceOracleAPI/inventory/ping`)
-            .map((res: Response) => res.json());
-    }
-
     name: String = 'Sharath';
     lastName: String = 'R';
     cell: Number = 88892772944;
     imagepath: String = "https://blobstore-ls-ge.run.aws-usw02-pr.ice.predix.io/v1/blob/images.jpg";
+    people: any;
+    constructor(private http: Http) {
+    }
 
     getFullName(): string {
-        return this.name + '' + this.lastName;
+        return this.name + ' ' + this.lastName;
     }
+
+    fetchdata() {
+        console.log("Fetch Data")
+        return this.http.get(`http://localhost:8084/fieldEngineerProfile/ping`)
+            .map((res: Response) => res.json())
+            .subscribe(res => this.people = res);
+    }
+
 }
--- src/app/snippet/snippet.component.ts
+++ src/app/snippet/snippet.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { FECompoment } from '../fe/fe.component';
 
 @Component({
   selector: 'snippet-root',
   templateUrl: './snippet.component.html',
-  styleUrls: ['./snippet.component.css']
+  styleUrls: ['./snippet.component.css'],
+  providers: [FECompoment],
 })
 export class SnippetComponent {
   title2 = 'Code Snippet';
+  text :any;
+  
+  constructor(private fe: FECompoment) {
+  this.text=fe.getFullName();
+  }
+
 }
